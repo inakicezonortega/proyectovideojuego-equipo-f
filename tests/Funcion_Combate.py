4e58e704cc775e2291cfe979b39f1cd8e8e62c3b
carlos-castilla <61014078+carlos-castilla@users.noreply.github.com>
2020-05-14 19:11:37 +0200

    funcion combate con pokemon

--- tests/Funcion_Combate.py
+++ tests/Funcion_Combate.py
@@ -1,92 +1,75 @@
-def turno_aliado(entrenador, tipo_enemigo, hp_enemigo, def_enemigo):
+import random
+
+
+
+
+def turno_aliado_p(entrenador, pokemon, accion):
+
     while True:
 
-        accion = int(
-            input(print("¿Que accion quieres realizar?: \n (1): Cambiar de aliado \n (2): Usar objeto \n (3): Atacar")))
-
-        # # CAMBIAR # #
+        # # ATACAR # #
         if accion == 1:
-            cambiar_pokemon(entrenador)
-
-
-        # # OBJETOS # #
-        elif accion == 2:
-            objeto = int(input("¿Quieres huir o curarte?: \n (1): Huir \n (2): Pociones"))
-
-            #### Cuerda Huida ####
-            if objeto == 1:
-
-                # Si tiene en el inventario
-                if (entrenador.inventario["cuerda_huida"] > 0):
-
-                    # Si consigue huir
-                    if (huir()):
-
-                        entrenador.inventario["cuerda_huida"] = entrenador.inventario["cuerda_huida"] - 1
-                        print("Has huido con exito")
-                        return True # Salir del bucle
-
-                    # Si no consigue huir
-                    else:
-
-                        entrenador.lista_equipo["cuerda_huida"] = entrenador.inventario["cuerda_huida"] - 1
-                        print("No has conseguido huir")
-
-            #### Pocion ####
-            elif objeto == 2:
-
-                entrenador.lista_equipo[0].HP = pocion(entrenador.lista_equipo[0].HP, entrenador.lista_equipo[0].HP_MAX)
-
-
-        # #  ATACAR # #
-        elif accion == 3:
-
-            hp_enemigo = atacar(entrenador.lista_equipo[0].equipo, hp_enemigo, def_enemigo, tipo_enemigo,
-                                entrenador.lista_equipo[0].tipo)
+            
+            pokemon.HP = atacar(entrenador.lista_equipo[0].tipo, pokemon.tipo, pokemon.HP, pokemon.HP_MAX, entrenador.lista_equipo[0].ataque)
             return False     #Salir del bucle
 
-        # #  ERROR # #
-        else:
-            print("Elige una opcion valida")
+
+        # # POCION # #
+        elif accion == 2:
+            entrenador.lista_equipo[0].HP = pocion(entrenador, entrenador.lista_equipo[0].HP, entrenador.lista_equipo[0].HP_MAX)
+
+
+        # # CAMBIAR # #
+        elif accion == 3:
+
+            cambio = True
+            while not cambio:
+                num = 0
+                num += 1
+
+
+        # #  HUIR # #
+        elif accion == 4:
+            
+            # Si tiene en el inventario
+            if (entrenador.inventario["cuerda_huida"] > 0):
+
+                # Si consigue huir
+                if (huir(entrenador)):
+                    return True # Salir del bucle
 
 
 
 
 
 
-
-def combate(entrenador, tipo_enemigo, hp_enemigo, def_enemigo, ataque_enemigo):
+def combate_p(entrenador, pokemon, accion, combate):
 
     #Bucle principal
-    while True:
-
-
+    while combate:
 
 
         #Ataque enemigo
-        if (hp_enemigo != 0):
-            entrenador.lista_equipo[0].HP = atacar(ataque_enemigo, entrenador.lista_equipo[0].HP, entrenador.lista_equipo[0].defensa, entrenador.lista_equipo[0].tipo, tipo_enemigo)
+        if (pokemon.HP != 0):
+            entrenador.lista_equipo[0].HP = atacar(pokemon.tipo, entrenador.lista_equipo[0].tipo, entrenador.lista_equipo[0].HP, entrenador.lista_equipo[0].HP_MAX, pokemon.ataque)
 
-        elif (hp_enemigo == 0):
-            print ("Has ganado el combate")
-            #Añadir experiencia
+        #Gana el combate
+        elif (pokemon.HP == 0):
+            entrenador.lista_equipo[0].contador_exp = exp(entrenador.lista_equipo[0].contador_exp, entrenador.lista_equipo[0].nivel, pokemon.lista)
+            #Añadir texto
             break
-
-
-
-
 
         #Si el aliado sigue con vida
         if (entrenador.lista_equipo[0].HP != 0):
 
-            if (turno_aliado(entrenador, tipo_enemigo, hp_enemigo, def_enemigo)): break    #Si ha huido
-            else: continue                                                                 #Si no ha huido
+            if (turno_aliado_p(entrenador, pokemon, accion)): break    #Si ha huido
+            else: continue                                           #Si no ha huido
 
 
         #Si el aliado no sigue con vida
         elif (entrenador.lista_equipo[0].HP == 0):
 
-            print ("Aliado eliminado")
+
             #Retirar al pokemon
             entrenador.lista_muertos.append(entrenador.lista_equipo[0]) #Meter en la lista de muertos
             entrenador.lista_equipo.pop(entrenador.lista_equipo[0])     #Retirar del equipo de aliado
@@ -94,10 +77,271 @@ def combate(entrenador, tipo_enemigo, hp_enemigo, def_enemigo, ataque_enemigo):
             #Si quedan aliados vivos
             if (len(entrenador.lista_equipo[0] != 0)):
 
-                turno_aliado(entrenador, tipo_enemigo, hp_enemigo, def_enemigo)
+                turno_aliado_p(entrenador, pokemon, accion)
 
             #No quedan aliados vivos
             else:
-
-                print ("Todos tus aliados han muerto, has perdido el combate")
                 break
+
+
+
+def pocion(entrenador, vida_aliado, vida_maxima):
+    if vida_aliado != vida_maxima and entrenador["pocion"]!=0:
+        entrenador.inventario.pop("pocion")
+        return (vida_aliado * 1.5)
+
+    else: return vida_maxima
+
+
+
+def exp(exp_actual,lvl_aliado,lvl_enemigo):
+    #Comprobador de diferencia de niveles entre aliado y enemigo
+    dif_nivel = lvl_aliado - lvl_enemigo
+    #si nivel aliado es mayor->dara menos exp
+    #si nivel aliado es menor->dara mas exp
+    #si los niveles son iguales->dara un numero base de exp
+    if dif_nivel == 0:
+        exp_actual = exp_actual +  8
+    elif dif_nivel > 0:
+        exp_actual = exp_actual + 5
+    elif dif_nivel < 0:
+        exp_actual = exp_actual + 13
+
+    return exp_actual
+
+
+
+def huir(entrenador):
+
+    x = random.randrange(9)    #Numeros del 0 al 9
+
+    entrenador.lista_equipo["cuerda_huida"] = entrenador.inventario["cuerda_huida"] - 1
+
+    #La cuerda huida tiene un 30% de probabilidades de acertar, por lo tanto si x es 0, 1 o 2 surtira efecto
+    if x > -1 and x < 3: return True
+    else: return False
+
+
+def atacar(tipo_aliado , tipo_enemigo,hp_enemigo,hp_total,ataque):
+    if tipo_aliado == "demonio":
+        if tipo_enemigo == "demonio":  # demonio vs demonio
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "cometa":  # demonio vs cometa
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "volcanico":  # demonio vs volcanico
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "estelar":  # demonio vs estelar
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "vacio":  # demonio vs vacio
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "lunar":  # demonio vs lunar
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+    elif tipo_aliado == "cometa":
+        if tipo_enemigo == "demonio":  # cometa vs demonio
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "cometa":  # cometa vs cometa
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "volcanico":  # cometa vs volcanico
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "estelar":  # cometa vs estelar
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "vacio":  # cometa vs vacio
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "lunar":  # cometa vs lunar
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+    elif tipo_aliado == "volcanico":
+        if tipo_enemigo == "demonio":  # volcanico vs demonio
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "cometa":  # volcanico vs cometa
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "volcanico":  # volcanico vs volcanico
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "estelar":  # volcanico vs estelar
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "vacio":  # volcanico vs vacio
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "lunar":  # volcanico vs lunar
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+    elif tipo_aliado == "estelar":
+        if tipo_enemigo == "demonio":  # estelar vs demonio
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "cometa":  # estelar vs cometa
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "volcanico":  # estelar vs volcanico
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "estelar":  # estelar vs  estelar
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "vacio":  # estelar vs vacio
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "lunar":  # estelar vs  lunar
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+    elif tipo_aliado == "vacio":
+        if tipo_enemigo == "demonio":  # vacio vs demonio
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "cometa":  # vacio vs cometa
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "volcanico":  # vacio vs volcanico
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+        elif tipo_enemigo == "estelar":  # vacio vs estelar
+            if hp_enemigo > hp_total - ataque * 1.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 1.5
+
+        elif tipo_enemigo == "vacio":  # vacio vs vacio
+            if hp_enemigo > hp_total - ataque:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque
+
+        elif tipo_enemigo == "lunar":  # vacio vs lunar
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+    elif tipo_aliado == "lunar":
+        if tipo_enemigo == "demonio":  # lunar vs demonio
+            if hp_enemigo > hp_total - ataque * 0.5:
+                return hp_enemigo - 1
+            else:
+                return hp_total - ataque * 0.5
+
+    elif tipo_enemigo == "cometa":  # lunar vs cometa
+        if hp_enemigo > hp_total - ataque * 1.5:
+            return hp_enemigo - 1
+        else:
+            return hp_total - ataque * 1.5
+
+    elif tipo_enemigo == "volcanico":  # lunar vs volcanico
+        if hp_enemigo > hp_total - ataque:
+            return hp_enemigo - 1
+        else:
+            return hp_total - ataque
+
+    elif tipo_enemigo == "estelar":  # lunar vs estelar
+        if hp_enemigo > hp_total - ataque * 0.5:
+            return hp_enemigo - 1
+        else:
+            return hp_total - ataque * 0.5
+
+    elif tipo_enemigo == "vacio":  # lunar vs vacio
+        if hp_enemigo > hp_total - ataque * 1.5:
+            return hp_enemigo - 1
+        else:
+            return hp_total - ataque * 1.5
+
+    elif tipo_enemigo == "lunar":  # lunar vs lunar
+        if hp_enemigo > hp_total - ataque:
+            return hp_enemigo - 1
+        else:
+            return hp_total - ataque
+

b155a5445868bda84cdd68d873640edfdce8f702
carlos-castilla <61014078+carlos-castilla@users.noreply.github.com>
2020-05-14 19:48:37 +0200

    Añadida actualizacion de posicion

--- tests/Funcion_Combate.py
+++ tests/Funcion_Combate.py
@@ -1,17 +1,18 @@
+
 import random
 
-
-
+############## TURNO VS POKEMON ##############
 
 def turno_aliado_p(entrenador, pokemon, accion):
 
     while True:
 
-        # # ATACAR # #
-        if accion == 1:
-            
-            pokemon.HP = atacar(entrenador.lista_equipo[0].tipo, pokemon.tipo, pokemon.HP, pokemon.HP_MAX, entrenador.lista_equipo[0].ataque)
-            return False     #Salir del bucle
+        # # CAMBIAR # #
+        if accion == 3:
+            cambio = True
+            while not cambio:
+                num = 0
+                num += 1
 
 
         # # POCION # #
@@ -19,18 +20,9 @@ def turno_aliado_p(entrenador, pokemon, accion):
             entrenador.lista_equipo[0].HP = pocion(entrenador, entrenador.lista_equipo[0].HP, entrenador.lista_equipo[0].HP_MAX)
 
 
-        # # CAMBIAR # #
-        elif accion == 3:
-
-            cambio = True
-            while not cambio:
-                num = 0
-                num += 1
-
-
-        # #  HUIR # #
+        # # CUERDA HUIDA # #
         elif accion == 4:
-            
+
             # Si tiene en el inventario
             if (entrenador.inventario["cuerda_huida"] > 0):
 
@@ -39,11 +31,56 @@ def turno_aliado_p(entrenador, pokemon, accion):
                     return True # Salir del bucle
 
 
+        # #  ATACAR # #
+        elif accion == 1:
+
+            pokemon.HP = atacar(entrenador.lista_equipo[0].tipo, pokemon.tipo, pokemon.HP, pokemon.HP_MAX, entrenador.lista_equipo[0].ataque)
+            return False     #Salir del bucle
+
+
+
+############## TURNO VS ENTRENADOR ##############
+
+def turno_aliado_e(entrenador, rival, accion):
+
+    while True:
+
+        # # CAMBIAR # #
+        if accion == 3:
+            cambio = True
+            while not cambio:
+                num = 0
+                num += 1
+
+
+        # # POCION # #
+        elif accion == 2:
+            entrenador.lista_equipo[0].HP = pocion(entrenador, entrenador.lista_equipo[0].HP, entrenador.lista_equipo[0].HP_MAX)
+
+
+        # # CUERDA HUIDA # #
+        elif accion == 4:
+
+            # Si tiene en el inventario
+            if (entrenador.inventario["cuerda_huida"] > 0):
+
+                # Si consigue huir
+                if (huir(entrenador)):
+                    return True # Salir del bucle
+
+
+        # #  ATACAR # #
+        elif accion == 1:
+
+            rival.lista_equipo[0].HP = atacar(entrenador.lista_equipo[0].tipo, rival.lista_equipo[0].tipo, rival.lista_equipo[0].HP, rival.lista_equipo[0].HP_MAX, entrenador.lista_equipo[0].ataque)
+            return False     #Salir del bucle
 
 
 
 
-def combate_p(entrenador, pokemon, accion, combate):
+############## COMBATE VS POKEMON ##############
+
+def combate_p(entrenador, pokemon, accion, combate, room_anterior, x_anterior, y_anterior):
 
     #Bucle principal
     while combate:
@@ -55,14 +92,29 @@ def combate_p(entrenador, pokemon, accion, combate):
 
         #Gana el combate
         elif (pokemon.HP == 0):
-            entrenador.lista_equipo[0].contador_exp = exp(entrenador.lista_equipo[0].contador_exp, entrenador.lista_equipo[0].nivel, pokemon.lista)
-            #Añadir texto
+            entrenador.lista_equipo[0].contador_exp = exp(entrenador.lista_equipo[0].contador_exp, entrenador.lista_equipo[0].nivel, pokemon.nivel)
+            
+            # Volver a la habitacion anterior
+            self.current_room = room_anterior
+
+            # Coordenadas anteriores
+            self.player_sprite.center_x = x_anterior
+            self.player_sprite.center_y = y_anterior
             break
 
         #Si el aliado sigue con vida
         if (entrenador.lista_equipo[0].HP != 0):
 
-            if (turno_aliado_p(entrenador, pokemon, accion)): break    #Si ha huido
+            if (turno_aliado_p(entrenador, pokemon, accion)):
+
+                # Volver a la habitacion anterior
+                self.current_room = room_anterior
+
+                # Coordenadas anteriores
+                self.player_sprite.center_x = x_anterior
+                self.player_sprite.center_y = y_anterior
+                
+                break    #Si ha huido
             else: continue                                           #Si no ha huido
 
 
@@ -81,9 +133,94 @@ def combate_p(entrenador, pokemon, accion, combate):
 
             #No quedan aliados vivos
             else:
+
+                # Volver a la habitacion inicial
+                self.current_room = 1
+
+                # Coordenadas iniciales
+                self.player_sprite.center_x = 840
+                self.player_sprite.center_y = 120
+                
                 break
 
 
+
+############## COMBATE VS ENTRENADOR ##############
+
+def combate_e(entrenador, rival, accion, combate, room_anterior, x_anterior, y_anterior):
+    # Bucle principal
+    while combate:
+
+        # Ataque enemigo
+        if (rival.lista_equipo[0].HP != 0):
+            entrenador.lista_equipo[0].HP = atacar(rival.lista_equipo[0].tipo, entrenador.lista_equipo[0].tipo,
+                                                   entrenador.lista_equipo[0].HP, entrenador.lista_equipo[0].HP_MAX,
+                                                   rival.lista_equipo[0].ataque)
+
+        # Ganar el combate
+        elif (rival.lista_equipo[0].HP == 0):
+
+            # Si quedan enemigos vivos, cambiar el que combate
+            if (len(rival.lista_equipo[0] != 0)):
+                rival.lista_muertos.append(rival.lista_equipo[0])   #Añado a la lista de muertos
+                rival.lista_equipo.pop(0)   #Retirara de disponibles
+                
+                
+            # No quedan enemigos vivos
+            else:
+                entrenador.lista_equipo[0].contador_exp = exp(entrenador.lista_equipo[0].contador_exp,
+                                                              entrenador.lista_equipo[0].nivel, rival.lista_equipo[0].nivel)
+                # Volver a la habitacion anterior
+                self.current_room = room_anterior
+
+                # Coordenadas anteriores
+                self.player_sprite.center_x = x_anterior
+                self.player_sprite.center_y = y_anterior
+                break
+
+        # Si el aliado sigue con vida
+        if (entrenador.lista_equipo[0].HP != 0):
+
+            if (turno_aliado_p(entrenador, rival, accion)):
+                break  # Si ha huido
+            else:
+                continue  # Si no ha huido
+
+
+        # Si el aliado no sigue con vida
+        elif (entrenador.lista_equipo[0].HP == 0):
+
+            # Retirar al pokemon
+            entrenador.lista_muertos.append(entrenador.lista_equipo[0])  # Meter en la lista de muertos
+            entrenador.lista_equipo.pop(entrenador.lista_equipo[0])  # Retirar del equipo de aliado
+
+            # Si quedan aliados vivos
+            if (len(entrenador.lista_equipo[0] != 0)):
+
+                turno_aliado_p(entrenador, rival, accion)
+
+            # No quedan aliados vivos
+            else:
+                #Pierdes el combate, los pokemon enemigos vuelven a su estado inicial
+                for pokemon_muerto in rival.lista_muertos:
+                    pokemon_muerto.HP = pokemon_muerto.HP_MAX   #Cura al pokemon
+                    rival.lista_equipo.append(pokemon_muerto)   #Lo añade a los disponibles
+                    rival.lista_muertos.pop(pokemon_muerto)     #Lo retira de lista de muertos
+
+                # Volver a la habitacion inicial
+                self.current_room = 1   
+
+                # Coordenadas iniciales
+                self.player_sprite.center_x = 840 
+                self.player_sprite.center_y = 120
+                
+                break   #Termina el combate
+
+
+
+
+
+############## FUNCIONES NECESARIAS PARA EL COMBATE ##############
 
 def pocion(entrenador, vida_aliado, vida_maxima):
     if vida_aliado != vida_maxima and entrenador["pocion"]!=0:
